315f74834ef74d8475e1151381a90eb28a6a62c7
Valentin Jubert <valentin.jubert@ideine.fr>
2018-03-06 14:01:50 +0100

    Début debug solution

--- Problem.cs
+++ Problem.cs
@@ -1,56 +1,91 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace HashCode2018
 {
 	public class Problem
 	{
 		public int Bonus { get; set; }
-		
+
 		public int StepCount { get; set; }
-		
-		public List<Ride> Rides { get; set; }
-		
-		public List<Vehicle> Vehicles { get; set; }
+
+		public List<Ride> Rides { get; }
+
+		public List<Vehicle> Vehicles { get; }
 
 		public Problem()
 		{
 			Rides = new List<Ride>();
-			Vehicles =new List<Vehicle>();
+			Vehicles = new List<Vehicle>();
 		}
 	}
 
 	public class Ride
 	{
-		public int Id { get; set; }
-		
-		public int StartX { get; set; }
-		
-		public int StartY { get; set; }
-		
-		public int EndX { get; set; }
-		
-		public int EndY { get; set; }
-		
-		public int StartStep { get; set; }
-		
-		public int EndStep { get; set; }
-		
-		public int Distance { get; set; }
+		public int Id { get; }
+		public int StartX { get; }
+		public int StartY { get; }
+		public int EndX { get; }
+		public int EndY { get; }
+		public int StartStep { get; }
+		public int EndStep { get; }
+		public int Distance { get; }
+
+		public bool Done { get; set; }
+
+		public List<(int, Ride)> NextRideList { get; }
+
+		public Ride(int id, int startX, int startY, int endX, int endY, int startStep, int endStep)
+		{
+			Id = id;
+			StartX = startX;
+			StartY = startY;
+			EndX = endX;
+			EndY = endY;
+			StartStep = startStep;
+			EndStep = endStep;
+			Distance = Math.Abs(startX - endX) + Math.Abs(startY - endY);
+			NextRideList = new List<(int, Ride)>();
+		}
+
+		public void AddNextRide(Ride r)
+		{
+			NextRideList.Add((DistanceToRide(r), r));
+		}
+
+		public int DistanceToRide(Ride r)
+		{
+			return Math.Abs(EndX - r.StartX) + Math.Abs(EndY - r.StartY);
+		}
+
+		public bool IsUsable(Vehicle vehicle)
+		{
+			return !Done && vehicle.CurrentStep + vehicle.CurrentRide.DistanceToRide(this) + Distance < EndStep;
+		}
 	}
 
 	public class Vehicle
 	{
 		public int CurrentStep { get; set; }
-		
-		public bool Finished { get; set; }
-		
-		public List<int> Rides { get; set; }
 
-		public Node Node { get; set; }
-		
+		public Ride CurrentRide { get; set; }
+
+		public List<Ride> Rides { get; }
+
 		public Vehicle()
 		{
-			Rides = new List<int>();
+			Rides = new List<Ride>();
+		}
+
+		public void ToNextRide(Ride nextRide)
+		{
+			var distanceToNextRide = CurrentRide.DistanceToRide(nextRide);
+			var distanceOfNextRide = nextRide.Distance;
+			var attente = Math.Max(nextRide.StartStep - (CurrentStep + distanceToNextRide), 0);
+			CurrentStep += distanceOfNextRide + attente + distanceToNextRide;
+			Rides.Add(nextRide);
+			CurrentRide = nextRide;
+			nextRide.Done = true;
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Security;
 
 namespace HashCode2018
 {
@@ -9,13 +10,13 @@ namespace HashCode2018
 	{
 		private const string FILE = "a_example.in";
 
-		private static List<string> _files = new List<string>
+		private static readonly List<string> _files = new List<string>
 		{
-			"a_example.in",
+			//"a_example.in",
 			"b_should_be_easy.in",
-			"c_no_hurry.in",
-			"d_metropolis.in",
-			"e_high_bonus.in"
+			//"c_no_hurry.in",
+			//"d_metropolis.in",
+			//"e_high_bonus.in"
 		};
 
 		static void RunFile(string file)
@@ -29,15 +30,12 @@ namespace HashCode2018
 
 			Output(file, problem.Vehicles);
 
-			Console.WriteLine("Done!");
+			Console.WriteLine($"Done {file}!");
 		}
 
 		static void Main(string[] args)
 		{
-			foreach (string file in _files)
-			{
-				RunFile(file);
-			}
+			_files.AsParallel().ForAll(RunFile);
 		}
 
 		static Problem Input(string file)
@@ -48,25 +46,16 @@ namespace HashCode2018
 			List<int> values = lines[0].Split(' ').Select(int.Parse).ToList();
 			Problem result = new Problem
 			{
-				Vehicles = Enumerable.Range(0, values[2]).Select(_ => new Vehicle()).ToList(),
 				Bonus = values[4],
 				StepCount = values[5]
 			};
+			result.Vehicles.AddRange(Enumerable.Range(0, values[2]).Select(_ => new Vehicle()));
 
 			int counter = 0;
 			foreach (string line in lines.Skip(HEADER_COUNT))
 			{
 				values = line.Split(' ').Select(int.Parse).ToList();
-				result.Rides.Add(new Ride
-				{
-					Id = counter++,
-					StartX = values[0],
-					StartY = values[1],
-					EndX = values[2],
-					EndY = values[3],
-					StartStep = values[4],
-					EndStep = values[5],
-				});
+				result.Rides.Add(new Ride(counter++, values[0], values[1], values[2], values[3], values[4], values[5]));
 			}
 
 			return result;
@@ -74,7 +63,7 @@ namespace HashCode2018
 
 		static void Output(string file, List<Vehicle> data)
 		{
-			List<string> lines = data.Select(x => $"{x.Rides.Count} {string.Join(" ", x.Rides)}").ToList();
+			List<string> lines = data.Select(x => $"{x.Rides.Count} {string.Join(" ", x.Rides.Select(ride => ride.Id))}").ToList();
 
 			WriteFile(file, lines);
 		}
@@ -90,211 +79,56 @@ namespace HashCode2018
 		}
 	}
 
-	public class Node
-	{
-		public Node()
-		{
-			Links = new List<(int, Node)>();
-		}
-
-		public bool Done { get; set; }
-
-		public Ride Ride { get; set; }
-
-		public bool IsStart { get; set; }
-
-		public List<(int, Node)> Links { get; set; }
-	}
-
 	public class Solution
 	{
 		private readonly Problem _problem;
-		private int _currentStep = 0;
 
 		public Solution(Problem problem)
 		{
 			_problem = problem;
-
-			_problem.Rides.ForEach(x => x.Distance = Distance(x));
-		}
-
-		private Node CreateNode()
-		{
-			Node startNode = new Node
-			{
-				Ride = null,
-			};
-
-			List<Node> endNodes = new List<Node>();
-			startNode.Links.AddRange(_problem.Rides.Select(ride =>
-			{
-				Node startRide = new Node
-				{
-					Ride = ride,
-					IsStart = true,
-				};
-
-				startRide.Links.Add((ride.Distance, new Node
-				{
-					Ride = ride,
-					IsStart = false,
-				}));
-				endNodes.Add(startRide.Links[0].Item2);
-				return (DistanceStart(ride), startRide);
-			}));
-
-			foreach (Node startTrip in startNode.Links.Select(x => x.Item2))
-			{
-				foreach (Node endTrip in endNodes)
-				{
-					if (startTrip.Ride != endTrip.Ride)
-					{
-						endTrip.Links.Add((Distance(endTrip.Ride, startTrip.Ride), startTrip));
-					}
-				}
-			}
-
-			return startNode;
 		}
 
 		public void Solve()
 		{
-			Node start = CreateNode();
-			_problem.Vehicles.ForEach(x => x.Node = start);
-
-			List<Vehicle> vehicles = _problem.Vehicles.ToArray().ToList();
-			for (int stepIndex = 0; stepIndex < _problem.StepCount; stepIndex++)
+			//Init
+			var startRide = new Ride(-1, 0, 0, 0, 0, 0, 1);
+			foreach (var currentRide in _problem.Rides)
 			{
-				_currentStep = stepIndex;
-				for (var j = 0; j < vehicles.Count; j++)
+				startRide.AddNextRide(currentRide);
+				foreach (var ride in _problem.Rides)
 				{
-					Vehicle v = vehicles[j];
+					currentRide.AddNextRide(ride);
+				}
+			}
 
-					if (v.CurrentStep > stepIndex)
+			//Tour de jeu
+			foreach (var vehicle in _problem.Vehicles)
+			{
+				vehicle.CurrentRide = startRide;
+				vehicle.CurrentStep = 0;
+
+				while (true)
+				{
+					var usableRide = vehicle.CurrentRide.NextRideList.Where(tuple => tuple.Item2.IsUsable(vehicle));
+					var (distance, nextRide) = usableRide.OrderBy(tuple => Score(tuple.Item2, vehicle, _problem.Bonus)).FirstOrDefault();
+					if (nextRide == null)
 					{
-						continue;
+						break;
 					}
 
-					AffectVehicle(v);
-
-					if (v.Finished)
-					{
-						vehicles.RemoveAt(j);
-						j--;
-					}
+					vehicle.ToNextRide(nextRide);
 				}
 			}
 		}
 
-		public void AffectVehicle(Vehicle v)
+		public int Score(Ride ride, Vehicle vehicle, int bonusPoint)
 		{
-			Node next = null;
-			int distanceToNode = 0;
-			int maxScore = -100_000_000;
-
-			Node start = v.Node;
-			foreach ((int distance, Node node) in start.Links)
-			{
-				if (IsUsable(start, distance, node))
-				{
-					int result = Score(start, distance, node);
-
-					if (maxScore < result)
-					{
-						distanceToNode = distance;
-						maxScore = result;
-						next = node;
-					}
-				}
-			}
-
-			if (next == null)
-			{
-				v.Finished = true;
-			}
-			else
-			{
-				v.Rides.Add(next.Ride.Id);
-
-				v.CurrentStep += distanceToNode + next.Ride.Distance;
-
-				//add waiting time before ride
-				int arrival = _currentStep + distanceToNode;
-				if (arrival < next.Ride.StartStep)
-				{
-					v.CurrentStep += next.Ride.StartStep - arrival;
-				}
-
-				//mark start & end node as "Done"
-				v.Node = next.Links[0].Item2;
-				next.Done = true;
-				v.Node.Done = true;
-			}
-		}
-
-		public bool IsUsable(Node x, int distance, Node y)
-		{
-			if (y.Done)
-			{
-				return false;
-			}
-
-			if (y.IsStart)
-			{
-				int tripDistance = y.Ride.Distance;
-				int arrival = _currentStep + distance;
-
-				//add time before beginning
-				int add = 0;
-				if (arrival < y.Ride.StartStep)
-				{
-					add = y.Ride.StartStep - arrival;
-				}
-
-				if (arrival + add + tripDistance >= y.Ride.EndStep)
-				{
-					return false;
-				}
-			}
-
-			return true;
-		}
-
-		public int Score(Node x, int distance, Node y)
-		{
-			int tripDistance = y.Ride.Distance;
-			int arrival = _currentStep + distance;
-			int add = 0;
-			//add time before ride begin
-			if (arrival < y.Ride.StartStep)
-			{
-				add = y.Ride.StartStep - arrival;
-			}
-
-			//add bonus point
-			int bonus = 0;
-			if (arrival <= y.Ride.StartStep)
-			{
-				bonus = _problem.Bonus;
-			}
-
-			// trajet + bonus - (temps avant démarrage)
-			return tripDistance + bonus - (distance + add);
-		}
-
-		public static int DistanceStart(Ride r)
-		{
-			return Math.Abs(r.StartX) + Math.Abs(r.StartY);
-		}
-
-		public static int Distance(Ride r1, Ride r2)
-		{
-			return Math.Abs(r1.EndX - r2.StartX) + Math.Abs(r1.EndY - r2.StartY);
-		}
-
-		public static int Distance(Ride r)
-		{
-			return Math.Abs(r.EndX - r.StartX) + Math.Abs(r.EndY - r.StartY);
+			var distanceToNextRide = vehicle.CurrentRide.DistanceToRide(ride);
+			var distanceOfRide = ride.Distance;
+			var deltaStart = ride.StartStep - (vehicle.CurrentStep + distanceToNextRide);
+			var attente = Math.Max(deltaStart, 0);
+			var bonus = deltaStart == 0 ? bonusPoint : 0;
+			return distanceOfRide + bonus - attente - distanceToNextRide;
 		}
 	}
 }
